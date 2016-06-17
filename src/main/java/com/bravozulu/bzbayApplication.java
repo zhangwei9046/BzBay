fc6f163c52b10b990a88b7b6328f82b2a9151f5b
Mark Bonicillo <bonicma@Marks-MacBook-Air.local>
2016-06-16 18:35:10 -0700

    initial commit

---
+++ src/main/java/com/bravozulu/bzbayApplication.java
@@ -0,0 +1,37 @@
+package com.bravozulu;
+
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.migrations.MigrationsBundle;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class bzbayApplication extends Application<bzbayConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new bzbayApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "bzbay";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<bzbayConfiguration> bootstrap) {
+        bootstrap.addBundle(new MigrationsBundle<bzbayConfiguration>() {
+            @Override
+            public DataSourceFactory getDataSourceFactory(bzbayConfiguration
+                                                                  configuration) {
+                return configuration.getDataSourceFactory();
+            }
+        });
+    }
+
+    @Override
+    public void run(final bzbayConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
