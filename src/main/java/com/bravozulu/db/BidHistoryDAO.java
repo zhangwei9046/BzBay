756f4840a09d46a65203c07a88e2ac7e05dcf373
MelodyL <melodylovepug@gmail.com>
2016-07-13 18:23:35 -0700

    transaction,bithistory(core,migration,db)

--- src/main/java/com/bravozulu/db/BidHistoryDAO.java
+++ src/main/java/com/bravozulu/db/BidHistoryDAO.java
@@ -1,9 +1,11 @@
 package com.bravozulu.db;
 
 import com.bravozulu.core.BidHistory;
+import com.bravozulu.core.Transactions;
 import io.dropwizard.hibernate.AbstractDAO;
 import org.hibernate.SessionFactory;
 
+import javax.ws.rs.NotFoundException;
 import java.util.Optional;
 
 /**
@@ -13,8 +15,22 @@ public class BidHistoryDAO extends AbstractDAO<BidHistory>{
     public BidHistoryDAO(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
+
+
+    public BidHistory create(BidHistory bidhistory) { return persist(bidhistory); }
+
     public Optional<BidHistory> findBybidId(long id) {
         return Optional.ofNullable(get(id));
     }
-    public void deleteBidHistory(long id) {}
+
+    public void delete(Long bidId) {
+        BidHistory bid = findBybidId(bidId).orElseThrow(() -> new NotFoundException("No such bid."));
+        currentSession().delete(bid);
+    }
+
+
+    public BidHistory update(BidHistory bid,Long bidId,) {
+        bid.setBidId(bidId);
+        return persist(bidId);
+    }
 }
