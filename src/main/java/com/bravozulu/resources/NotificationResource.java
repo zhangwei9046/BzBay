80c2acae24f88cfba1b3ecccc6483c29cfdedf30
Mark Bonicillo <bonicma@Marks-MacBook-Air.local>
2016-08-09 14:46:49 -0700

    Added import NotificationDAO

--- src/main/java/com/bravozulu/resources/NotificationResource.java
+++ src/main/java/com/bravozulu/resources/NotificationResource.java
@@ -9,6 +9,7 @@ import com.bravozulu.db.BidHistoryDAO;
 import com.bravozulu.db.UserDAO;
 import com.bravozulu.db.TransactionsDao;
 import com.bravozulu.core.Notification;
+import com.bravozulu.db.NotificationDao;
 import com.codahale.metrics.annotation.Timed;
 import io.dropwizard.auth.Auth;
 import io.dropwizard.hibernate.UnitOfWork;
