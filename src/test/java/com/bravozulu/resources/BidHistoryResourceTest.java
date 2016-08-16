a6e0ff704230fab2619c7d67524e548c39aec567
Mark Bonicillo <bonicma@Marks-MacBook-Air.local>
2016-08-16 11:17:04 -0700

    Adding final Resource tests

--- src/test/java/com/bravozulu/resources/BidHistoryResourceTest.java
+++ src/test/java/com/bravozulu/resources/BidHistoryResourceTest.java
@@ -1,8 +1,9 @@
 package com.bravozulu.resources;
 
 import com.bravozulu.auth.BzbayAuthenticator;
-import com.bravozulu.core.Item;
+import com.bravozulu.core.BidHistory;
 import com.bravozulu.core.User;
+import com.bravozulu.db.BidHistoryDAO;
 import com.bravozulu.db.ItemDAO;
 import com.bravozulu.db.UserDAO;
 import io.dropwizard.auth.AuthDynamicFeature;
@@ -21,73 +22,71 @@ import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.reset;
 
-/**
- * Created by bonicma on 8/16/16.
- */
-public class ItemResourceTest {
+
+public class BidHistoryResourceTest {
+    private static final BidHistoryDAO bhDAO = mock(BidHistoryDAO.class);
     private static final ItemDAO itemDAO = mock(ItemDAO.class);
     private static final UserDAO userDAO = mock(UserDAO.class);
-    private static final BzbayAuthenticator auth = Mockito.mock
-            (BzbayAuthenticator.class);
+    private static final BzbayAuthenticator auth = mock (BzbayAuthenticator
+            .class);
 
     @ClassRule
     public static final ResourceTestRule resources =
             ResourceTestRule.builder().setTestContainerFactory(new GrizzlyWebTestContainerFactory())
                     .addProvider(new AuthDynamicFeature(new BasicCredentialAuthFilter.Builder<User>()
-                            .setAuthenticator(ItemResourceTest.auth).setRealm
+                            .setAuthenticator(BidHistoryResourceTest.auth).setRealm
                                     ("Validate User")
                             .setPrefix("Basic").buildAuthFilter()))
                     .addProvider(RolesAllowedDynamicFeature.class)
                     .addProvider(new AuthValueFactoryProvider.Binder<>(User.class))
-                    .addResource(new ItemResource(itemDAO, userDAO))
+                    .addResource(new BidHistoryResource(bhDAO, itemDAO, userDAO))
                     .build();
 
     @Captor
-    private ArgumentCaptor<Item> itemCaptor = ArgumentCaptor.forClass(Item
-            .class);
-    private Item item1;
-    private Item item2;
+    private ArgumentCaptor<BidHistory> bidHistoryArgumentCaptor =
+            ArgumentCaptor.forClass(BidHistory.class);
+    private BidHistory bh1;
+    private BidHistory bh2;
+
 
     @Before
     public void setUp() throws Exception {
-        item1.setItemId(42);
 
     }
 
     @After
     public void tearDown() throws Exception {
-        reset(itemDAO);
-    }
-
-    @Test
-    public void findAllItems() throws Exception {
 
     }
 
     @Test
-    public void create() throws Exception {
+    public void bid() throws Exception {
 
     }
 
     @Test
-    public void findAllAvailableItems() throws Exception {
+    public void findAllBidHistory() throws Exception {
 
     }
 
     @Test
-    public void search() throws Exception {
+    public void findBidById() throws Exception {
 
     }
 
     @Test
-    public void findItemByName() throws Exception {
+    public void findBidByUserId() throws Exception {
 
     }
 
     @Test
-    public void findItemById() throws Exception {
+    public void findBidByItemId() throws Exception {
+
+    }
+
+    @Test
+    public void findHighestPrice() throws Exception {
 
     }
 
